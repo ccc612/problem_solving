f14e0526efcc1c3ef9f1cc2415ea49909c3bd2c2
Minchul Jung <ccc612@gmail.com>
2015-10-22 00:45:23 +0900

    (hand_coding) adds code file for solve hackerrank "the-binary-search-efficiency-doubter" problem

---
+++ hand_coding/the-binary-search-efficiency-doubter.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static long bsCount(long size) {
+        long count = 0;
+        long nextNodeCount = 1;
+        long i;
+        for (i = 1; size > nextNodeCount; ++i) {
+            count += (nextNodeCount * i);
+            size -= nextNodeCount;
+            nextNodeCount *= 2;
+        }
+        
+        if (size > 0)
+            count += (size * i);
+        
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long size = 0;
+        for(int i = 1; scan.hasNextLong(); ++i) {
+            System.out.println("Case " + i + ": " + bsCount(scan.nextLong()));
+        }
+    }
+}
